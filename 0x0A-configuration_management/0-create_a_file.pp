02e6f54eac5f0e61652d842303d962f9a330584f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-09-22 02:55:50 -0700

    schoolcreated

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+$doc_root = '/tmp'
+
+# Ensure the /tmp/school directory exists with the specified permissions and ownership.
+file { $doc_root:
+  ensure => 'directory',
+}
+
+# Create the /tmp/school file with the desired content.
+file { "${doc_root}/school":
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  require => File[$doc_root],
+}
