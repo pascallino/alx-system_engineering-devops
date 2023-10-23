18a7fc18ac23670dc8b3e22442c57aac962af5ab
root <root@DESKTOP-GTI1M9R>
2023-10-23 14:26:58 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,35 +5,26 @@ import requests
 import sys
 
 
-def fetchdata(employeeid):
+def fetchdata(user_id):
     """ do json file from aspi"""
-    if employeeid is None:
+    if user_id is None:
         return
 
-    baseurl = f'https://jsonplaceholder.typicode.com/todos?userId={employeeid}'
-    usersurl = f'https://jsonplaceholder.typicode.com/users/{employeeid}'
-    user_rep = requests.get(usersurl)
-    user_data = user_rep.json()
-    if user_rep.status_code != 200:
-        return
-
-    base_rep = requests.get(baseurl)
-    base_data = base_rep.json()
-    tott = [task for task in base_data]
-    username = user_data['username']
-    userid = user_data['id']
-
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    tott = requests.get(url + "todos", params={"userId": user_id}).json()
     result = {}
-    result[str(userid)] = []
+    result[str(user_id)] = []
     for task in tott:
-        result[str(userid)].append({
+        result[str(user_id)].append({
             "task": task["title"],
             "completed": task["completed"],
             "username": username
         })
 
     # Now, let's save the data to a JSON file
-    filename = f'{userid}.json'
+    filename = f'{user_id}.json'
 
     with open(filename, 'w') as jsonfile:
         json.dump(result, jsonfile)
